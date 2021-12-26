2d10395d4509023953e37603040b727b9c6ac94b
Asier Cardoso <asier314@gmail.com>
2021-12-26 14:29:00 +0100

    elplacerdepensar intro

--- el_placer_de_pensar/el_placer_de_pensar.py
+++ el_placer_de_pensar/el_placer_de_pensar.py
@@ -1,22 +1,69 @@
 from manim import *
+import manimpango
 
 class Palette:
-    DARK = "#072448"
-    BLUE = "#54d2d2"
-    YELLOW = "#ffcb00"
-    ORANGE = "#f8aa4b"
-    RED = "#ff6150"
+    BACKGROUND = "#ece6e2"
 
-    GREEN = "#54d254"
+class PortalPalette:
+        LIGHT_ORANGE = "#ff9a00"
+        ORANGE = "#ff5d00"
+        BLACK = "#000000"
+        BLUE = "#0065ff"
+        LIGHT_BLUE = "#00a2ff"
 
 class ElPlacerDePensarV1(Scene):
     def construct(self):
-        self.wait()
-        el_placer_de_pensar = Tex(
-            "El Placer de Pensar"
-        )
-        self.play(Write(el_placer_de_pensar, run_time=1))
-        self.wait(1)
-        self.play(FadeOut(el_placer_de_pensar))
-        self.wait()
+        # self.wait()
+        # el_placer_de_pensar = Tex(
+        #     "El $\mathbb{P}$lacer de $\mathbb{P}$ensar}", color=Palette.BLACK
+        # )
+        # self.play(Write(el_placer_de_pensar, run_time=1))
+        # self.wait(1)
+        # self.play(FadeOut(el_placer_de_pensar))
+        # self.wait()
 
+
+        placer = Text(
+            "El Placer",
+            color=BLACK,
+            gradient=(PortalPalette.BLUE, PortalPalette.LIGHT_BLUE),
+            font="TeX Gyre Pagella Math",
+            # text2color={"El": PortalPalette.LIGHT_BLUE, " Placer": PortalPalette.BLUE}
+        ).scale(2).move_to(LEFT)
+
+        p = Text(
+            "P",
+            color=PortalPalette.LIGHT_BLUE,
+            font="TeX Gyre Pagella Math",
+        ).scale(3)
+
+        pensar = Text(
+            "de Pensar",
+            color=BLACK,
+            font="TeX Gyre Pagella Math",
+            gradient=(PortalPalette.ORANGE, PortalPalette.LIGHT_ORANGE),
+            # text2color={"de": PortalPalette.LIGHT_ORANGE, " Pensar": PortalPalette.ORANGE}
+        ).scale(2).next_to(placer, RIGHT)
+        circle = Circle(
+            color=PortalPalette.LIGHT_BLUE,
+            fill_color=PortalPalette.LIGHT_ORANGE,
+            fill_opacity=1,
+            gradient=(PortalPalette.LIGHT_BLUE, PortalPalette.LIGHT_ORANGE)
+        ).surround(p, buffer_factor=1.5)
+
+        VGroup(placer, pensar).move_to(ORIGIN)
+        placer_copy = placer.copy()
+
+        self.play(Write(placer), run_time=1)
+        self.add(placer_copy)
+        self.wait(0.5)
+        self.play(TransformMatchingShapes(placer, pensar, run_time=2, path_arc=PI/2))
+        self.wait(1)
+
+        self.remove(placer_copy)
+        self.play(
+            TransformMatchingShapes(placer, p),
+            Transform(pensar, circle),
+            run_time=1.5
+        )
+        self.wait(5)
